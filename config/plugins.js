21d92ab9bd9bf08e3d15daa59e14a8db57b703b1
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2024-01-27 22:01:36 +0100

    update]

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+      apiToken: "o474EEhuN8eJfuKMfTKfMpD9",
+      appFilter: "trademindshub-blog",
+      teamFilter: "your-team-id-on-vercel",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
